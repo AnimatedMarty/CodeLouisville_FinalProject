df65de4ffbdc16aebf783d40d635cd2b2e4b2199
AnimateMarty <mlindsey@martinlindsey.com>
2016-07-17 11:54:39 -0400

    Moved styles and img folders into a public folder.

--- FirstDraftCodeInExpress/server.js
+++ FirstDraftCodeInExpress/server.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
+app.use('/public', express.static(__dirname + '/public'));
+
+// I don't think I need this line.//
+//Will delete in the end if app works without it.//
 /* var router = express.Router(); */
+
+// This sequence of commands routes to each web page//
 var path = __dirname + '/views/';
 
 app.use(function (req,res,next) {
@@ -20,6 +26,9 @@ app.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
 });
 
+
+
+// I don't think I need this line. Will delete in the end if app works without it.//
 /* app.use("/",router); */
 
 app.use("*",function(req,res){
